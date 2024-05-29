6b2f11c5cdfa19f832999cdac872875add61f949
SongYibao <songybmail@foxmail.com>
2024-05-29 17:37:26 +0800

    doc: 增加debug信息

--- http_client/http_client.c
+++ http_client/http_client.c
@@ -9,7 +9,8 @@ size_t on_response_callback(char *ptr, size_t size, size_t nmemb, void *userdata
 {
     neu_plugin_t *plugin = (neu_plugin_t *) userdata;
     plugin->common.link_state = NEU_NODE_LINK_STATE_CONNECTED;
-    plog_debug(plugin, "http请求结束回调函数");
+    plog_debug(plugin,"http请求结束回调函数");
+    plog_debug(plugin,"算法服务器返回数据: %s",ptr);
     return size * nmemb;
 }
 // 发送HTTP POST请求的函数，包括URL, JSON数据和回调函数
